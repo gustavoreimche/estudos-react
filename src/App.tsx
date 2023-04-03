a6a8de87bf5f985e0e096bb7298105d41cd46311
Gustavo Reimche <gustavoreimche@hotmail.com>
2023-04-03 17:11:17 -0300

    Criando eventos

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,11 @@ import { Header } from './components/Header'
 import { Photo } from './components/Photo'
 
 const App = () => {
+
+  const handleButtonClick = () =>{
+    alert("O botão foi clicado");
+  }
+
   return (
     <div>
       <Header title="Este é um exemplo" />
@@ -11,6 +16,8 @@ const App = () => {
       <Photo legend='google img'>
         <img src="http://www.google.com/google.jpg" alt="" />
       </Photo>
+
+      <button onClick={handleButtonClick}>Clique aqui</button>
 
     </div>
   )
